6876d89308a4c561deaa3b62329a3df1705a5ecc
Roberto Júnior <robertojunior085@gmail.com>
2023-01-18 23:25:22 -0300

    Add zod and create POST /goals route

--- server/src/routes/goals.ts
+++ server/src/routes/goals.ts
@@ -1,5 +1,7 @@
 import type { FastifyInstance } from 'fastify'
 
+import { z } from 'zod'
+
 import { prisma } from '../db/prisma'
 
 export async function goals(app: FastifyInstance) {
@@ -7,5 +9,23 @@ export async function goals(app: FastifyInstance) {
     const goals = await prisma.goal.findMany()
 
     return reply.header('X-Total-Count', goals.length).send(goals)
+  })
+
+  app.post('/', async (request, reply) => {
+    const requestBodySchema = z.object({
+      description: z.string(),
+      recurrences: z.array(z.number().min(0).max(6))
+    })
+
+    const { description, recurrences } = requestBodySchema.parse(request.body)
+
+    const goal = await prisma.goal.create({
+      data: {
+        description,
+        recurrences: { create: recurrences.map((week_day) => ({ week_day })) }
+      }
+    })
+
+    return reply.status(201).send(goal)
   })
 }
